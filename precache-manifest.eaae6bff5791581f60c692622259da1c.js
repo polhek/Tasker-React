a81e3c7c60e9f727bbc119fd0c833cfacf638859
polhek <ziga.groselj@gmail.com>
2020-10-12 18:45:29 +0200

    Updates

---
+++ precache-manifest.eaae6bff5791581f60c692622259da1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e932a218d0867b26a056a4bf9f1f0df9",
+    "url": "/Tasker-React/index.html"
+  },
+  {
+    "revision": "cb7cbe659ccf90d490b7",
+    "url": "/Tasker-React/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "662caf777adfbae952f9",
+    "url": "/Tasker-React/static/js/2.066eb761.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Tasker-React/static/js/2.066eb761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb7cbe659ccf90d490b7",
+    "url": "/Tasker-React/static/js/main.12db03da.chunk.js"
+  },
+  {
+    "revision": "07b656b843e29412aa03",
+    "url": "/Tasker-React/static/js/runtime-main.74d6b342.js"
+  }
+]);
